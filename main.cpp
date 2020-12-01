5e671dd8c2d3fd11eb55c2d36263708780e7dc41
Théo Mirabile <theo.mirabile@me.com>
2020-12-01 08:46:30 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : main.cpp
+Auteur(s) : Théo Mirabile
+Date creation : <jj.mm.aaaa>
+Description : <à compléter>
+Remarque(s) : <à compléter>
+Compilateur : Apple clang version 12.0.0
+----------------------------------------------------------------------------2-------
+*/
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main() {
+
+ return EXIT_SUCCESS;
+}
